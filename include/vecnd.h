170d34855c4b4ea9dea56336b5232aaaf19c7003
ZEYI TAO <zeyitao@ZEYIs-MacBook-Air.local>
2016-11-06 01:15:20 -0400

    vecnd modification

--- include/vecnd.h
+++ include/vecnd.h
@@ -20,13 +20,14 @@ class vecnd{
 	// Data Member //
 	/////////////////
 	std::unique_ptr<value_type[]> _attrs;
+	size_t _size;
 
 	public:
 	////////////////
 	// Constructor//
 	////////////////
 	vecnd(void);
-	vecnd(int size);
+	vecnd(size_t size);
 	vecnd(const vecnd& v);
 
 	//vecnd(std::unique_ptr<value_type[]> v);
@@ -38,14 +39,19 @@ class vecnd{
 	iterator begin(void);
 	const_iterator begin(void) const;
 	iterator end(void);
-	const_iterator end(void)const;	
+	const_iterator end(void)const;
+
+	size_t size(void) const {return _size;};
 	///////////////
 	// Operators //
 	///////////////
+	reference operator[](size_t index);
+
 	vecnd& operator=(const vecnd& v);
+	vecnd& operator=(vecnd&& v);
 
 	bool operator == (const vecnd& v) const;
-	bool operator != (const vecnd& v) const;
+	//bool operator != (const vecnd& v) const;
 
 	vecnd operator-(void) const;
 
@@ -67,17 +73,43 @@ class vecnd{
 	// Methods   //
 	///////////////
 	value_type dot(const vecnd& v) const;
-
-	vecnd cross(const vecnd& v) const;
+	value_type squared_length(void) const;
+	value_type length(void) const;
 	///////////////
 	// Modifiers //
 	///////////////
 	vecnd& abs(void);
 	vecnd& normalize(void);
+
+	/////////////
+	// Friends //
+	/////////////
+	friend void swap(vecnd& a, vecnd& b){
+		return a._swap(b);
+	}
+	friend vecnd abs(const vecnd& v){
+		return vecnd(v).abs();
+	}
+	friend vecnd normalize(const vecnd& v){
+		return vecnd(v).normalize();
+	}
+	friend vecnd operator*(const_reference scale, const vecnd& v){
+		return (v*scale);
+	}
+	friend std::ostream& operator<<(std::ostream& s, const vecnd& v){
+		s<<"[";
+		for(const_iterator i=v.begin();i!=v.end();i++){
+			s<<*i<<",";
+		}
+		s<<"]"<<std::endl;
+		return s;
+	}
 	private:
 	///////////////
 	// Private   //
 	///////////////
+	void _assign(const vecnd& v);
+	void _swap(vecnd& v);
 };
 
 #endif
--- src/vecnd.cpp
+++ src/vecnd.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+
 #include "vecnd.h"
 
 /////////////////
@@ -6,35 +7,72 @@
 /////////////////
 vecnd::vecnd(void){
 	_attrs = NULL;
+	_size =0;
 }
 
-vecnd::vecnd(int size){
+vecnd::vecnd(size_t size){
 	_attrs.reset(new value_type[size]);
+	_size = size;
 }
 vecnd::vecnd(const vecnd& v){
 	std::copy(v.begin(), v.end(), begin());
+	_size = v._size;
 }
 
 ////////////////
 // Inspectors //
 ////////////////
 vecnd::iterator vecnd::begin(void){
-	return _attrs.begin();
+	return _attrs.get();
 }
 vecnd::iterator vecnd::end(void){
-	return _attrs.end();
+	return begin()+size();
 }
 
 vecnd::const_iterator vecnd::begin(void) const{
-	return _attrs.begin();
+	return _attrs.get();
 }
 vecnd::const_iterator vecnd::end(void) const{
-	return _attrs.end();
+	return begin()+size();
 }
 
 //////////////
 // Operator //
 //////////////
+vecnd::reference vecnd::operator[](size_t index){
+	return _attrs[index];
+}
+
+vecnd& vecnd::operator=(const vecnd& v){
+	_assign(v);
+	return *this;
+}
+
+vecnd& vecnd::operator=(vecnd&& v){
+	_swap(v);
+	return*this;
+}
+
+bool vecnd::operator==(const vecnd& v) const{
+	if(size() != v.size()){
+		return false;
+	}
+	for(size_t i=0; i < size(); i++){
+		if(_attrs[i] != v._attrs[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+vecnd vecnd::operator-(void) const{
+	vecnd result;
+	for (int i = 0; i < size(); i++){
+		result._attrs[i] = _attrs[i] * -1;
+	}
+	return result;
+}
+
 vecnd vecnd::operator+(const vecnd& v) const {
 	vecnd result;
 	std::transform(begin(), end(), v.begin(), result.begin(),[](const_reference a, const_reference b)
@@ -117,25 +155,67 @@ vecnd& vecnd::operator/=(const vecnd& v){
 	return *this;
 }
 vecnd& vecnd::operator*=(vecnd::value_type scale){
-	std::for_each(begin(), end(), [&](const_reference a)
+	std::for_each(begin(), end(), [&](reference a)
 	{
-		return a*scale;
+		a*=scale;
 	});
 	return *this;
 }
 vecnd& vecnd::operator/=(vecnd::value_type scale){
-	std::for_each(begin(), end(), [&](const_reference a)
+	std::for_each(begin(), end(), [&](reference a)
 	{
-		return a/scale;
+		a/=scale;
 	});
 	return *this;
+}
+/////////////
+// Method  //
+/////////////
+vecnd::value_type vecnd::dot(const vecnd& v) const{
+	vecnd::value_type dot=0.0f;
+	vecnd result;
+	std::transform(begin(), end(), v.begin(), result.begin(),[](const_reference a, const_reference b)
+	{
+		return a*b;
+	});
+	std::for_each(result.begin(), result.end(), [&](reference a)
+	{
+		dot+=a;
+	});
+	return dot;
+}
+
+vecnd::value_type vecnd::squared_length(void) const{
+	return dot(*this);
+}
+
+vecnd::value_type vecnd::length(void) const{
+	return sqrt(squared_length());
 }
 
 //////////////
 // Modifier //
 //////////////
+vecnd& vecnd::abs(void){
+	std::for_each(begin(), end(), [](reference val){
+		if(val < 0) val = -val;
+	});
+	return *this;
+}
 
+vecnd& vecnd::normalize(void){
+	*this /= length();
+	return *this;
+}
 
 /////////////
 // Private //
 /////////////
+void vecnd::_assign(const vecnd& v){
+	std::copy(v.begin(), v.end(), begin());
+	_size = v._size;
+}
+void vecnd::_swap(vecnd& v){
+	std::swap(_attrs,v._attrs);
+	std::swap(_size, v._size);
+}
