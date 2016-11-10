6f93a62773c01561c2d63b3ce33b0da6913b73ec
zxdan523 <xzhu08@email.wm.edu>
2016-11-09 22:30:50 -0500

    test

--- bin/testListnd.cpp
+++ bin/testListnd.cpp
@@ -1,8 +1,12 @@
 #include "listnd.h"
+#include "vecnd.h"
 #include <iostream>
 
 int main()
 {
+	vecnd v(3);
+	v[0]=1;v[1]=2;v[2]=3;
+	std::cout<<v<<std::endl;
 	listnd lt(10);
 	for(int i=0;i<10;i++)
 	{
---
+++ bin/testRandomProjection.cpp
@@ -0,0 +1,6 @@
+#include "abod.h"
+
+int main()
+{
+	return 0;
+}
